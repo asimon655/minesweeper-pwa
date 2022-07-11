037c7081d8b3578f1e6fb3db29655c9a2cdc39c0
asimon <avrahams655@gmail.com>
2022-07-11 14:14:05 +0300

    change adapter

--- svelte.config.js
+++ svelte.config.js
@@ -8,8 +8,20 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter({
+		  // default options are shown. On some platforms
+		  // these options are set automatically — see below
+		  pages: 'build',
+		  assets: 'build',
+		  fallback: null,
+		  precompress: false
+		}),
+	
+		prerender: {
+		  // This can be false if you're using a fallback (i.e. SPA mode)
+		  default: true
+		}
+	  }
 };
 
 export default config;
